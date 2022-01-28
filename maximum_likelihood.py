be7e1da2505bf5f14725865e6d34a75952249f4a
Z. Zhang <zheng_zhang1@alumni.brown.edu>
2022-01-28 08:53:32 +0200

    Try "Newton-CG" method

--- maximum_likelihood.py
+++ maximum_likelihood.py
@@ -8,7 +8,7 @@ configfile = "/data/zzhang/sim1/bt_matrices/config.yaml"
 pipeline_info = Parameters_collection.from_config(configfile) # Fetch info about the telescope and former steps
 
 
-CV = Covariances(0,0.3,2,0,0.15,2,pipeline_info['dk_0thresh_fg_3thresh'])
+CV = Covariances(0,0.3,2,0,0.15,2,pipeline_info['dk_1thresh_fg_3thresh'])
 
 testdata = N.arange(200)
 test = Likelihood(testdata, CV)
@@ -26,7 +26,7 @@ def Hessian(pvec):
         test(pvec)
         return test.hess
     
-res = minimize(log_likelihood, p0, jac= Jacobian, hess=Hessian)
+res = minimize(log_likelihood, p0, method='Newton-CG', jac= Jacobian, hess=Hessian)
 
 textfile = open("mlpse.txt", "w")
 for element in list(res.x):
